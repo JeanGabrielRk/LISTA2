a1ab2134800fb2d302f6aaa721bed2ce6acc75a4
Jean <jeangabriel058@gmail.com>
2024-03-16 15:36:33 -0300

    Exercicio 1

---
+++ Exercicio01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+    public static void executar(){
+
+        Scanner leitor = new Scanner(System.in);
+
+        int i;
+        double numeros[] = new double[5];
+        double soma = 0;
+        double media;
+
+        for (i = 0; i < 5; i++){
+
+            System.out.println("Digite um numero: ");
+            numeros[i] = leitor.nextDouble();
+
+            soma = soma + numeros[i];
+        }
+
+        media = soma / 5;
+
+        System.out.println("A media eh: " + media);
+        
+    }
+
+}
