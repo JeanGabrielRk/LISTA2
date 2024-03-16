378edc558230949f9a8a61f48bfabdc51335319c
Jean <jeangabriel058@gmail.com>
2024-03-16 15:46:06 -0300

    Exercicio 2

--- Exercicio02.java
+++ Exercicio02.java
@@ -8,20 +8,25 @@ public class Exercicio02 {
 
         int i;
         double numeros[] = new double[5];
-        double soma = 0;
-        double media;
 
         for (i = 0; i < 5; i++){
 
             System.out.println("Digite um numero: ");
             numeros[i] = leitor.nextDouble();
-
-            soma = soma + numeros[i];
         }
 
-        media = soma / 5;
+        for (i = 0; i < 5; i++){
 
-        System.out.println("A media eh: " + media);
+            if (numeros[i] > 0){
+                System.out.println("O numero eh positivo na posicao: " + i);
+            }
+            else if (numeros[i] < 0){
+                System.out.println("O numero eh negativo na posicao: " + i);
+            }
+            else if (numeros[i] == 0){
+                System.out.println("O numero eh igual a zero na posicao: " + i);
+            }
+        }
         
     }
 
